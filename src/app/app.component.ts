23eb5e8e21fbbd0250ce2913b3afe38163514e54
Abubakar Usman <abusmoh@gmail.com>
2017-02-22 08:10:37 +0100

    Debugged no of trials

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,14 @@ export class AppComponent {
   randNumber: number;
   guessedNumber: number;
   deviation: number;
-  noOfTries: number;
+  trials: number;
 
   constructor(){
     this.initializeGame();
   }
 
   initializeGame(){
-    this.noOfTries = 0;
+    this.trials = 0;
     this.randNumber = Math.floor((Math.random() * 1000) + 1);
     this.guessedNumber = null;
     this.deviation = null;
@@ -24,7 +24,7 @@ export class AppComponent {
 
   verifyGuess(){
       this.deviation = this.randNumber - this.guessedNumber;
-      this.noOfTries += 1;
+      this.trials += 1;
   }
 
 }
